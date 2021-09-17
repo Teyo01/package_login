0c1122678b2f3a89df8d9065f10dcac243da67d6
Nicolas <nicolasehrmann@yahoo.fr>
2021-09-17 11:23:02 +0200

    update welcome page

--- client/view/welcome.vue
+++ client/view/welcome.vue
@@ -1,8 +1,6 @@
 <template>
   <div>
     <h1>{{$t('welcome')}}</h1>
-    <q-btn @click="increment">Click Me</q-btn>
-    <p>You've pressed the button {{counter}} times.</p>
   </div>
 </template>
 
@@ -10,19 +8,8 @@
 export default {
   data() {
     return {
-      counter: 0,
-    }
-  },
-  methods: {
-    increment() {
-      this.counter += 1
     }
   },
 }
 </script>
 
-<style scoped>
-  p {
-    font-family: serif;
-  }
-</style>
